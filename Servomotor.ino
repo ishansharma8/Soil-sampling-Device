0576d0793d100ccd726e08e9d536cc2f6ad09824
Ishan_4 <68964460+ishansharma8@users.noreply.github.com>
2022-08-24 12:51:14 +0530

    Create Servomotor.ino

---
+++ Servomotor.ino
@@ -0,0 +1,25 @@
+ #include<Servo.h>
+Servo Myservo;
+int pos;
+void setup()
+{
+Myservo.attach(9);
+}
+
+void loop()
+{
+  
+  
+for(pos=0;pos<=180;pos++){
+Myservo.write(pos);
+delay(5);
+}
+  delay(500);
+  
+  for(pos=180;pos>=0;pos--){
+Myservo.write(pos);
+delay(5);
+}
+  delay(500);
+  
+}
